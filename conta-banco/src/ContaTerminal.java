5c9b4c342aee2d2cfd60d88743890211eeebf377
DF-S <106790259+DF-S@users.noreply.github.com>
2023-06-15 02:49:10 +0000

    atualização 14-06 adquirindo dados e imprimindo

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Bem vindo ao auto-atendimento bank-bank");
+        System.out.println("");
+        
+        // Criando um objeto CadastroCliente
+        CadastroCliente cadastro = new CadastroCliente();
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Digite o número da Agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o número da conta:");
+        int numero = scanner.nextInt();
+
+        System.out.println("Digite quanto deseja depositar:");
+        double saldo = scanner.nextDouble();
+
+        // Atribuindo os valores aos atributos do objeto cadastro
+        cadastro.nome = nome;
+        cadastro.agencia = agencia;
+        cadastro.numero = numero;
+
+        cadastro.saldo = saldo;
+
+        System.out.println("Dados do Cliente:");
+        cadastro.imprimirDadosCliente();
+    }
+}
